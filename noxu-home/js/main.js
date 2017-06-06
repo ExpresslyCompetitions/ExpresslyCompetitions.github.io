392ca78643942e3620b282feec9c1187509c5995
jacubsmith <jakesmith1922@googlemail.com>
2017-06-06 17:17:15 +0100

    Removed old project files

--- noxu-home/js/main.js
+++
@@ -1,8 +0,0 @@
-if (form.on("click", "input", function() {
-  var t = $(this),
-    n = t.attr("id");
-  "undefined" == typeof n && (n = t.attr("name")),
-  siteTracking.runTrackingCommand("event", "form", "inputField", n),
-  sectionElement.hasClass("writing") === !1 && sectionElement.addClass("writing"),
-  "xly-subscribe" !== t.attr("id") && e(t, !0)
-})
